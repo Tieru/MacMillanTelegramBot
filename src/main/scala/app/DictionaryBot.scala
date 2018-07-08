74a13111b421b22e3fff031cc05b70c143ca9a06
Slavik Voronov <voronov.vchs@gmail.com>
2018-07-08 14:04:12 +0700

    Akka client, proxy support

--- src/main/scala/app/DictionaryBot.scala
+++ src/main/scala/app/DictionaryBot.scala
@@ -1,18 +1,20 @@
 package app
 
+import java.net.{InetSocketAddress, Proxy}
+
 import com.google.inject.Guice
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.Logger
 import di.ApplicationModule
+import info.mukel.telegrambot4s.api._
 import info.mukel.telegrambot4s.api.declarative.Commands
-import info.mukel.telegrambot4s.api.{Extractors, Polling, TelegramBot}
+import info.mukel.telegrambot4s.clients.{AkkaHttpClient, ScalajHttpClient}
 import info.mukel.telegrambot4s.models.InlineQuery
 import response.entry.EntryResponseHandler
 import response.search.SearchResponseHandler
 import response.{BotInlineQueryContext, BotMessageContext, InlineQueryContext, MessageContext}
 
-
-object DictionaryBot extends TelegramBot with Polling with Commands {
+object DictionaryBot extends AkkaTelegramBot with Polling with Commands {
 
   private val botLogger = Logger("Bot")
 
@@ -21,6 +23,16 @@ object DictionaryBot extends TelegramBot with Polling with Commands {
   private val injector = Guice.createInjector(new ApplicationModule())
 
   override def token: String = ConfigFactory.load().getString("bot.token")
+
+  override val client: RequestHandler = {
+    val token = ConfigFactory.load().getString("bot.token")
+    val configs = ConfigFactory.load()
+    if (configs.hasPath("bot.useProxy") && configs.getBoolean("bot.useProxy")) {
+      new ScalajHttpClient(token, buildProxySettings())
+    } else {
+      new AkkaHttpClient(token)
+    }
+  }
 
   onCommand("/word") { implicit msg =>
     withArgs { args =>
@@ -42,4 +54,18 @@ object DictionaryBot extends TelegramBot with Polling with Commands {
   }
 
   botLogger.info("Bot started successfully")
+
+  private def buildProxySettings(): Proxy = {
+    val configs = ConfigFactory.load()
+    var host = configs.getString("bot.proxyHost")
+
+    if (configs.hasPath("bot.proxyUser")) {
+      val proxyUser = configs.getString("bot.proxyUser")
+      val proxyPassword = configs.getString("bot.proxyPassword")
+      host = s"$proxyUser:$proxyPassword@$host"
+    }
+
+    val port = configs.getInt("bot.proxyPort")
+    new Proxy(Proxy.Type.HTTP, InetSocketAddress.createUnresolved(host, port))
+  }
 }
